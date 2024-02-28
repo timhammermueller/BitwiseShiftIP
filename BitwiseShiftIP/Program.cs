44d59484923be3aab64308d430849c25364a5c4a
timhammermueller <19273713+timhammermueller@users.noreply.github.com>
2024-02-28 08:19:53 +0100

    Changed folder name to match project name

--- BitwiseShiftIP/Program.cs
+++ BitwiseShiftIP/Program.cs
@@ -8,11 +8,11 @@ namespace BitwiseShiftIP
         static void Main(string[] args)
         {
             var localAddress = new NetAddress(192, 168, 178, 1);
-            var subnet = new SubnetMask(16);
+            var subnet = new SubnetMask(24);
             var networkAddress = localAddress.CalculateNetworkAddress(subnet);
             var firstHost = networkAddress + 1;
             var secondHost = networkAddress + 2;
-            var lastDevice = networkAddress + subnet.CalculateHostCount();
+            var lastHost = networkAddress + subnet.CalculateHostCount();
             var broadcast = networkAddress.CalculateBroadcastAddress(subnet);
         }
     }
